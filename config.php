04045511f45376e4f87262f8acd7443a1b007894
moycat <mxq.1203@gmail.com>
2015-08-09 13:52:10 +0800

    历史遗留……
    因为学校的服务器关了，所以可能有一段时间无法更新……

---
+++ config.php
@@ -0,0 +1,17 @@
+﻿<?php
+// 数据库配置
+$sqlhost = "127.0.0.1";
+$sqlname = "scorelooker";
+$sqlpw = "scorelooker";
+$sqlbase = "scorelooker";
+
+// 信息配置
+$host = "score.neko";	// 阅卷服务器地址
+$port = "8081";	// 阅卷端口
+$mac = "000C290C5636";	// 虚拟MAC地址
+
+// 文件存放目录
+$catchFile = "get";	// 临时文件目录
+$picture = "pic";	// 得到的图片存放目录
+
+?>
